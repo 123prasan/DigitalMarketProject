1fb4289b69a214c284f8dbb6a92b7d7d2fe4eabd
Prasanna <prasannaprasanna35521@gmail.com>
2025-10-03 01:05:13 +0530

    Update: ejs files, new routes, and route folder file

--- models/userDownloads.js
+++ models/userDownloads.js
@@ -1,10 +1,18 @@
-const { text } = require("body-parser");
-const mongoose=require("mongoose");
-const userdownloads=new mongoose.Schema({
-    userId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
-    filename:{type:String,required:true},
-    fileId:{type:mongoose.Schema.Types.ObjectId,ref:"File",required:true,unique:true},
-    fileUrl:{type:String,required:true},
-    fileType:{type:String,required:true}
-},{timestamps:true});
-module.exports=mongoose.model("userdownloads",userdownloads)
+const mongoose = require("mongoose");
+
+const userdownloads = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    filename: { type: String, required: true },
+    fileId: { type: mongoose.Schema.Types.ObjectId, ref: "File", required: true },
+    fileUrl: { type: String, required: true },
+    fileType: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+// Compound unique index: one user can't download same file twice,
+// but different users can download the same file
+userdownloads.index({ userId: 1, fileId: 1 }, { unique: true });
+
+module.exports = mongoose.model("userdownloads", userdownloads);
