04fdb28b4c3d15914d80874c1cae681821ea3103
Prasanna <prasannaprasanna35521@gmail.com>
2025-10-03 01:12:42 +0530

    Update: ejs files, new routes, and route folder file

--- models/userDownloads.js
+++ models/userDownloads.js
@@ -1,17 +1,10 @@
-const mongoose = require("mongoose");
-
-const userdownloadsSchema = new mongoose.Schema(
-  {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    filename: { type: String, required: true },
-    fileId: { type: mongoose.Schema.Types.ObjectId, ref: "File", required: true },
-    fileUrl: { type: String, required: true },
-    fileType: { type: String, required: true },
-  },
-  { timestamps: true }
-);
-
-// compound index
-userdownloadsSchema.index({ userId: 1, fileId: 1 }, { unique: true });
-
-module.exports = mongoose.model("userdownloads", userdownloadsSchema);
+const { text } = require("body-parser");
+const mongoose=require("mongoose");
+const userdownloads=new mongoose.Schema({
+    userId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
+    filename:{type:String,required:true},
+    fileId:{type:mongoose.Schema.Types.ObjectId,ref:"File",required:true},
+    fileUrl:{type:String,required:true},
+    fileType:{type:String,required:true}
+},{timestamps:true});
+module.exports=mongoose.model("userdownloads",userdownloads)
